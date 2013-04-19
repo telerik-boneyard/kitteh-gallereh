86551eab49ffb46859ce57597767f911167d179a
Derick Bailey <derickbailey@gmail.com>
2013-04-18 20:37:47 -0500

    trying to get more mvvmish with it

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,21 +1,7 @@
 ImageGallery.Controller = (function(){
 
-  var galleryLayout = kendo.observable({
-    selectedImageId: 0,
-
-    show: function(){
-      this.layout = new kendo.Layout("gallery-layout-template");
-      this.layout.render("body")
-    },
-
-    showContent: function(view){
-      this.layout.showIn("#main", view);
-    },
-
-    showList: function(view){
-      this.layout.showIn("#image-list", view);
-    }
-
+  var obj = kendo.observable({
+    selectedImageId: 0
   });
 
   var Controller = {
@@ -25,48 +11,54 @@ ImageGallery.Controller = (function(){
 
       var that = this;
       this.imageSource.bind("change", function(){
-        that.showImageById(galleryLayout.get("selectedImageId"));
+        that.showImageById(obj.get("selectedImageId"));
       });
 
       ImageGallery.ImageList.init(imageDataSource);
       ImageGallery.AddEdit.init(imageDataSource);
 
-      galleryLayout.show();
+      this.showLayout();
       this.showImageList();
     },
 
     addImage: function(){
       var addImageView = ImageGallery.AddEdit.getAddImageView();
-      galleryLayout.showContent(addImageView);
+      this.layout.showIn("#main", addImageView);
     },
 
     editImageById: function(id){
       this._getById(id, function(image){
         var editImageView = ImageGallery.AddEdit.getEditImageView(image);
-        galleryLayout.showContent(editImageView);
+        this.layout.showIn("#main", editImageView);
       });
     },
 
     showImageById: function(id){
       this._getById(id, function(image){
         var showImageView = ImageGallery.ImageViewer.getImageView(image);
-        galleryLayout.showContent(showImageView);
+        this.layout.showIn("#main", showImageView);
       });
+    },
+
+    showLayout: function(){
+      this.layout = new kendo.Layout("gallery-layout-template");
+      this.layout.render("body")
     },
 
     showImageList: function(){
       var imageListView = ImageGallery.ImageList.getImageListView();
-      galleryLayout.showList(imageListView);
+      this.layout.showIn("#image-list", imageListView);
     },
 
     _getById: function(id, callback){
       var image = this.imageSource.get(id);
-      galleryLayout.set("selectedImageId", id);
+      obj.set("selectedImageId", id);
 
       if (image){
         callback.call(this, image);
       }
     }
+
   };
 
   return Controller;
--- public/js/imagelist.js
+++ public/js/imagelist.js
@@ -4,23 +4,21 @@ ImageGallery.ImageList = (function(){
     imageCount: 0,
 
     scrollingSize: function(){
-      console.log("scroll size update");
       var imageCount = this.get("imageCount");
       var size = imageCount * 160;
       return size + "px";
     },
 
     imageClicked: function(e){
-      e.preventDefault();
-      var id = $(e.currentTarget).data("id");
-      ImageGallery.Router.navigate("/images/" + id);
+      var index = e.sender.select().index();
+      var item = this.imageSource.view()[index];
+      console.log(item);
     },
 
     setImageSource: function(imageDataSource){
       var that = this;
       this.imageSource = imageDataSource;
       this.imageSource.bind("change", function(){
-        console.log("data source change", this.view().length);
         that.set("imageCount", this.view().length);
       });
     }
